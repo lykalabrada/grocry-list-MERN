f5b7600c809de65124be113f2a3d025374463811
Lyka Labrada <lyka.l@tech.ducatus.net>
2018-11-14 09:25:32 +0800

    Remove requirement of Redux dev tools

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -26,13 +26,13 @@ class AppNavbar extends Component {
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarBrand href="/">Grocery Shopping List</NavbarBrand>
             <NavbarToggler onClick={this.toggle}></NavbarToggler>
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://google.com">
-                    Google
+                  <NavLink href="https://github.com/lykalabrada">
+                    Lyka's Github
                   </NavLink>
                 </NavItem>
               </Nav>
